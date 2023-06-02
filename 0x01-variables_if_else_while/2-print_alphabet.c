9e8583dd4da037684627e999b1d83a04e29d85dc
BodeMurairi <bodemurairi2@gmail.com>
2023-06-02 02:02:56 -0700

    If...While

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() 
+{
+    char alphabet = 'a';
+    while (alphabet <= 'z')
+    {
+        putchar(alphabet);
+        alphabet ++;
+    }
+    putchar('\n');
+    
+    return 0;
+}
