3b68fdda19904f05b0acea74cbf02a0335e7f3b4
BodeMurairi <bodemurairi2@gmail.com>
2023-07-04 16:38:01 -0700

    C more singly lists

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,52 +1,41 @@
-#include "lists.h"
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include "lists.h"
 
 /**
- * find_listint_loop - function that finds the loop in a linked list.
- * @head: pointer to the head of linked list.
+ * main - check the code
  *
- * You are not allowed to use malloc, free or arrays.
- * You can only declare a maximum of two variables in your function.
- *
- * Return: The address of the node where the loop starts, or NULL if,
- * there is no loop.
+ * Return: Always 0.
  */
-
-listint_t *find_listint_loop(listint_t *head)
+int main(void)
 {
-	/* Use of Floyd’s Cycle-Finding Algorithm */
-	/* let slow_ptr and fast_ptr be two node pointers pointing to the head node */
-	listint_t *slow_ptr = head, *fast_ptr = head;
-	/* if list is empty or has only one node without a loop return NULL */
-	if (head == NULL || head->next == NULL)
-		return (NULL);
-	/* in every iteration slow_ptr ptr moves ahead by one node */
-	slow_ptr = slow_ptr->next;
-	/* in every iteration fast_ptr ptr moves ahead by two nodes */
-	fast_ptr = fast_ptr->next->next;
-	/* search for a loop using slow_ptr and fast_ptr pointers */
-	while (fast_ptr && fast_ptr->next)
-	{
-		/**
-		 * slow_ptr and fast_ptr eventually meet at the same node, thus,
-		 * indicating that the linked list contains a loop.
-		 */
-		if (slow_ptr == fast_ptr)
-			break;
-		slow_ptr = slow_ptr->next;
-		fast_ptr = fast_ptr->next->next;
-	}
-	/* if loop does not exists return NULL*/
-	if (slow_ptr != fast_ptr)
-		return (NULL);
-	/* if loop exists, start slow_ptr from head and fast_ptr from meet point */
-	slow_ptr = head;
-	while (slow_ptr != fast_ptr)
-	{
-		slow_ptr = slow_ptr->next;
-		fast_ptr = fast_ptr->next;
-	}
-	/* return slow_ptr */
-	return (slow_ptr);
+    listint_t *head;
+    listint_t *head2;
+    listint_t *node;
+
+    head2 = NULL;
+    add_nodeint(&head2, 0);
+    add_nodeint(&head2, 1);
+    add_nodeint(&head2, 2);
+    add_nodeint(&head2, 3);
+    add_nodeint(&head2, 4);
+    add_nodeint(&head2, 98);
+    add_nodeint(&head2, 402);
+    add_nodeint(&head2, 1024);
+    print_listint_safe(head2);
+    head = NULL;
+    node = add_nodeint(&head, 0);
+    add_nodeint(&head, 1);
+    add_nodeint(&head, 2);
+    add_nodeint(&head, 3);
+    add_nodeint(&head, 4);
+    node->next = add_nodeint(&head, 98);
+    add_nodeint(&head, 402);
+    add_nodeint(&head, 1024);
+    print_listint_safe(head);
+    free_listint_safe(&head2);
+    free_listint_safe(&head);
+    printf("%p, %p\n", (void *)head2, (void *)head);
+    return (0);
 }
